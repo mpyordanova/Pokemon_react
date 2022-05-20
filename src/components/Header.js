e382d7d468e066f5a7123041bc53911d2644be43
Mariela <yordanova.mariela@gmail.com>
2022-05-20 09:24:29 -0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import css from './css/header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+        <h1>Gotta catch them all!</h1>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+const Home = (props) => {
+
+  const [apiResponse, setApiResponse] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`)
+      setApiResponse(response.data)
+    }
+    fetchData()
+  }, [])
+  
+  return (
+    <div className='Poke'>
+      <ul>
+        {
+          Object.keys(apiResponse).length > 0 ?
+            apiResponse.results.map((pokemon, index) => {
+              return(
+                <li key={index} onClick={() => {
+                  props.fetch(pokemon.url)
+                }}>{pokemon.name}</li>
+              )
+            })
+          : null
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default Home
